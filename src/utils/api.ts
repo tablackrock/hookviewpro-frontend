cf20aee5a882d62ca34b1d92c58819c4299b27c2
tablackrock <trevor@blackrock11.com>
2024-12-28 19:15:13 +0000

    api fix

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001",
-  //baseURL: "https://api.hookviewpro.com", // Update to production URL if applicable
+  //baseURL: "http://localhost:3001",
+  baseURL: "https://api.hookviewpro.com", // Update to production URL if applicable
   headers: {
     "Content-Type": "application/json",
   },
